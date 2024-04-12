629049b1a847d25debd02b5fe51c26e585fdcc16
AndrewZajac <andrewzajac05@gmail.com>
2024-04-12 14:46:33 -0500

    Worked on getting the cart project completed

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,15 +2,52 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+const maxItems = 5;
+
+// function addItem(item){
+//     if (isFull){}
+// }
+
+function addItem(item){
+    if(basket.length < maxItems){
+    basket.push(item);
+    console.log('added', item);
+    return true
+    }  else if(isFull() === true){
+        console.log(`Can not add, ${item}`);
+        return false;
+    }
+}
+addItem('apple');
+addItem('berries');
+addItem('book');
+addItem('flakes');
+addItem('bottle');
+addItem('chair');
 
 
+function listItems(){
+    for(let i = 0; i < basket.length; i ++){
+        console.log(basket[i]);
+    }
+    return true;
+}
 
+function empty(){
+    basket.splice(0, basket.length);
+}
+empty(basket);
+console.log(basket.length);
 
-
-
-
-
-
+function isFull(){
+    if (basket.length < maxItems){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 
 // DO NOT MODIFY
 // Used for automated testing
